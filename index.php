089ec781e2d8e0d0001979cea8c4df6eda33acfb
U-Joseph-THINK\Joseph <Joseph@Joseph-THINK.(none)>
2011-11-21 09:14:02 -0600

    working .js, .php files

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head><script type="text/javascript" src="script.js"></script></head>
+<body>
+Hello from HTML<br />
+<h1 id="testID">will be changed</h1>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+echo "hello from php";
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = setup;
+
+
+
+function setup(){
+var testp = document.getElementById("testID");
+testp.innerHTML="Hello from js";
+console.log("reached setup");
+
+}
